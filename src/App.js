651b7b572ec568928791558ec130d6df54be2e0e
BolajiOlajide <bolaji.olajide@andela.com>
2017-12-01 22:46:53 +0100

    create Home and Map component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
+// Import React and Component
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch,
+    Link
+} from 'react-router-dom'
 
+// Import the Home component to be used below
+import Home from './Home/Home'
+// Import the Maps component to be used below
+import Maps from './Maps/Maps'
+// Import the Blogs component to be used below
+import Blog from './Blog/Blog'
+// Import the NotFound component to be used below
+import NotFound from './NotFound/NotFound'
+ // Import CSS from App.css
+import './App.css';
+import createBrowserHistory from 'history/createBrowserHistory';
+
+const history = createBrowserHistory();
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render () {
+        return (
+            <Router history={history}>
+                <div>
+                    <header className="header">
+                        <nav className="navbar container">
+                            <div className="navbar-brand">
+                                <Link to="/">
+                                    <span className="navbar-item">Lazy Loading Routes</span>
+                                </Link>
+                            </div>
+
+                            <div className="navbar-end">
+                                <Link to="/maps">
+                                    <span className="navbar-item">Maps</span>
+                                </Link>
+                                <Link to="/blog">
+                                    <span className="navbar-item">Blog</span>
+                                </Link>
+                            </div>
+                        </nav>
+                    </header>
+                    <section className="content">
+                        <Switch>
+                            <Route exact path="/" component={Home} />
+                            <Route path="/maps" component={Maps} />
+                            <Route path="/blog" component={Blog} />
+                            <Route path="*" component={NotFound} />
+                        </Switch>
+                    </section>
+                </div>
+            </Router>
+        )
+    }
 }
 
 export default App;
